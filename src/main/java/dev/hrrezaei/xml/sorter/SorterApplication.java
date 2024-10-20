6c259f9e1b25b13f74bc149443adb8558ff2669d
Hamidreza Rezaei <hamidreza74hrr@yahoo.com>
2024-10-19 20:44:21 -0400

    Initial project structure + pom file

---
+++ src/main/java/dev/hrrezaei/xml/sorter/SorterApplication.java
@@ -0,0 +1,13 @@
+package dev.hrrezaei.xml.sorter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SorterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/hrrezaei/xml/sorter/SorterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hrrezaei.xml.sorter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
